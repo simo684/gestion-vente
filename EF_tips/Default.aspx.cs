c1ab16f5e25488a0e4ac1f3770e0f7ebec90342a
simo684 <attabousimo@hotmail.fr>
2019-12-19 10:06:07 +0100

    ajout du project

---
+++ EF_tips/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EF_tips
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                gestion_commandeEntities DB = new gestion_commandeEntities();
+                List<client> cli = DB.client.ToList();
+                if (cli.Count>0)
+                {
+                    foreach (var item in cli)
+                    {
+                        divDatos.InnerHtml += item.nom_cl + " " + item.prenom_cl + " - " + item.vill + "<br/>";
+                    }
+                }
+                else
+                {
+                    divDatos.InnerHtml = "no hay datos";
+                }
+            }
+        }
+    }
+}
